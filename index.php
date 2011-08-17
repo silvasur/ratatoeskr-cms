22e2c59b161c96fff4b883568596efa5b55285ef
Kevin Chabowski <kevin@kch42.de>
2011-08-18 01:25:40 +0200

    Rewriting rules (+testscript) and protection of system files.

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
 
-header("Content-Type: text/text");
+/*require_once(dirname(__FILE__) . "/ratatoeskr/main.php");
+ratatoeskr();*/
 
+header("Content-type: text/plain");
+print "\$_POST:\n";
+print_r($_POST);
+print "\n\n\$_GET:\n";
+print_r($_GET);
+print "\n\n\$_REQUEST:\n";
 print_r($_REQUEST);
-
+print "\n\n\$_SERVER:\n";
+print_r($_SERVER);
 ?>
