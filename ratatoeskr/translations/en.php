005ba07fb9b46191383adc583e80b0bddbbe05c8
Kevin Chabowski <kevin@kch42.de>
2011-10-12 22:01:02 +0200

    More translations

--- ratatoeskr/translations/en.php
+++ ratatoeskr/translations/en.php
@@ -38,7 +38,19 @@ $translation = array(
 	"e404_details" => "The page [[URL]] could not be found. Sorry.",
 	"author_name_missing" => "No author name given.",
 	"author_email_missing" => "No author E-Mailaddress given.",
-	"comment_text_missing" => "An empty comment can not be posted."
+	"comment_text_missing" => "An empty comment can not be posted.",
+	"comment_form_invalid" => "The comment form is invalid.",
+	"comment_too_fast" => "This comment was written too fast. Are you a spambot?",
+	"article_other_languages" => "This article in other languages: ",
+	"save_texts_as_lang" => "Save texts as language",
+	"settings_meta" => "Settings / Metadata",
+	"urlname" => "Unique URL name",
+	"tags_cs" => "Tags (comma seperated)",
+	"date_time" => "Date / Time",
+	"article_status" => "Article Status",
+	"article_status_hidden" => "Hidden",
+	"article_status_live" => "Live",
+	"article_status_sticky" => "Sticky"
 );
 
 ?>
