34809379563a6fc92889965441c56ba29c155947
Kevin Chabowski <kevin@kch42.de>
2012-06-15 22:23:26 +0200

    Filters in backend are now dynamic.
    If JavaScript is activated in the users browser, filterable tables are
    filtered immediatly.

--- ratatoeskr/js/backend.js
+++ ratatoeskr/js/backend.js
@@ -16,4 +16,42 @@ $(function()
 			$("div.articleeditor-metabar-element-content", self.parent()).show("fast");
 		}
 	});
+	
+	function filtertable(table, pairs)
+	{
+		$.each(pairs, function(idx, pair)
+		{
+			input = pair[0];
+			column = pair[1];
+			
+			(function(column){input.keyup(function()
+			{
+				filterby = $(this).val().toLowerCase();
+				$("tbody tr", table).each(function(i)
+				{
+					if($("td", this).eq(column).text().toLowerCase().indexOf(filterby) == -1)
+						$(this).hide()
+					else
+						$(this).show();
+				});
+			});})(column);
+		});
+	}
+	
+	$("#articlestable").each(function(i)
+	{
+		filtertable(
+			$("table", this),
+			[
+				[$("input[name=filter_urlname]", this), 1],
+				[$("input[name=filter_tag]",     this), 4],
+				[$("input[name=filter_section]", this), 5]
+			]
+		);
+	});
+	
+	$("#commentstable").each(function(i)
+	{
+		filtertable($("table", this), [[$("input[name=filter_article]", this), 7]]);
+	});
 });
