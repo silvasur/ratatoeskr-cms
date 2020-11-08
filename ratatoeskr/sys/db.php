dfa766f738959f0e857c26e7b8336e76db493c73
Laria Carolin Chabowski <laria@laria.me>
2020-11-08 17:22:39 +0100

    Remove unused functions/classes

--- ratatoeskr/sys/db.php
+++ ratatoeskr/sys/db.php
@@ -12,25 +12,12 @@
 
 use r7r\cms\sys\Database;
 use r7r\cms\sys\Env;
-use r7r\cms\sys\DbTransaction;
 
 if (!defined("SETUP")) {
     require_once(dirname(__FILE__) . "/../config.php");
 }
 
 require_once(dirname(__FILE__) . "/utils.php");
-
-/**
- * Substitutes "PREFIX_" in the input string with the prefix from the config.
- *
- * @param mixed|string $q
- * @return string
- * @deprecated Use {@see Database::subPrefix()} instead.
- */
-function sub_prefix($q): string
-{
-    return Env::getGlobal()->database()->subPrefix((string)$q);
-}
 
 /**
  * Prepares statement (1st argument) like {@see Database::prepStmt()} and executes it with the remaining arguments.
@@ -47,39 +34,4 @@ function qdb(...$args): PDOStatement
     }
 
     return Env::getGlobal()->database()->query((string)$args[0], ...array_slice($args, 1));
-}
-
-/**
- * Makes using transactions easier.
- *
- * @deprecated Use {@see DbTransaction} instead.
- */
-class Transaction
-{
-    /** @var DbTransaction */
-    private $tx;
-
-    /**
-     * Start a new transaction.
-     */
-    public function __construct()
-    {
-        $this->tx = new DbTransaction(Env::getGlobal()->database());
-    }
-
-    /**
-     * Commit the transaction.
-     */
-    public function commit(): void
-    {
-        $this->tx->commit();
-    }
-
-    /**
-     * Roll the transaction back.
-     */
-    public function rollback(): void
-    {
-        $this->tx->rollback();
-    }
 }
